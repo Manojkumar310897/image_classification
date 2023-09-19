309099768854af2d8caebae321f2635302a222e9
Manojravi3197 <manojravi3197@gmail.com>
2023-09-19 15:38:46 +0530

    Folder structure

--- template.py
+++ template.py
@@ -2,11 +2,12 @@ import os
 from pathlib import Path
 import logging
 
-logging.basicConfig(level=logging.INFO,format='[%(asctime)s]: %(message)s:')
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
-project_name="cnnClassifier"
 
-list_of_files=[
+project_name = "cnnClassifier"
+
+list_of_files = [
     ".github/workflows/.gitkeep",
     f"src/{project_name}/__init__.py",
     f"src/{project_name}/components/__init__.py",
@@ -18,28 +19,30 @@ list_of_files=[
     f"src/{project_name}/constants/__init__.py",
     "config/config.yaml",
     "dvc.yaml",
-    "params.py",
+    "params.yaml",
     "requirements.txt",
     "setup.py",
-    "research/trials.ipynb"
-    
+    "research/trials.ipynb",
+    "templates/index.html"
+
+
 ]
 
 
 for filepath in list_of_files:
-    filepath=Path(filepath)
-    filedir,filename=os.path.split(filepath)
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
 
 
-    if filedir!="":
-        os.makedirs(filedir,exist_ok=True)
-        logging.info(f"Creating Directory: {filedir} for the file: {filename}")
+    if filedir !="":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory; {filedir} for the file: {filename}")
+
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, "w") as f:
+            pass
+            logging.info(f"Creating empty file: {filepath}")
 
 
-        if(not os.path.exists(filepath))or (os.path.getsize(filepath)==0):
-            with open(filepath,"w")as f:
-                pass
-            logging.info("creating empty file: {filepath}")
-
-        else:
-            logging.info(f"{filename} is already exists")
+    else:
+        logging.info(f"{filename} is already exists")
